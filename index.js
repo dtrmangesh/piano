e3768ba08027a3f9e71bec2ac795f8e09dbb4fc2
Mangesh55 <mdatar@mobiquityinc.com>
2019-12-31 08:01:54 +0530

    Added Keypad key event for playing nodes.

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 const keys = document.querySelectorAll('.key');
-const whiteKeys = document.querySelectorAll('.key.white')
-const blackKeys = document.querySelectorAll('.key.black')
-
-keys.forEach(key => {
-  key.addEventListener('click', () => playNote(key))
-})
-
-document.addEventListener('keydown', e => {
-    if (e.repeat) return
-    const key = e.key
-    const whiteKeyIndex = WHITE_KEYS.indexOf(key)
-    const blackKeyIndex = BLACK_KEYS.indexOf(key)
-
-    if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
-    if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
-});
-    
+const WHITEKEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+const BLACKKEYS = ['s','d','g','h','j'];
+const whiteKey =document.querySelectorAll('.key.white')
+const blackKey = document.querySelectorAll('.key.black');    
 keys.forEach(key => {
     key.addEventListener('click', () => playNode(key));
 })
 
+document.addEventListener('keypress', e => {
+    const key = e.key;
+    const whiteKeyIndex = WHITEKEYS.indexOf(key);
+    const blackKeyIndex = BLACKKEYS.indexOf(key);
+    if (whiteKeyIndex > -1) playNode(whiteKey[whiteKeyIndex]);
+    if (blackKeyIndex > -1) playNode(blackKey[blackKeyIndex]);
+});
 
 const playNode = (key)=>{
     const nodeAudio = document.getElementById(key.dataset.node);
