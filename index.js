b153d72a83b340ca580fff682a8c7630d24af688
Mangesh55 <mdatar@mobiquityinc.com>
2019-12-31 07:40:10 +0530

    Added Keypad keys for listening events

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const keys = document.querySelectorAll('.key');
+const whiteKeys = document.querySelectorAll('.key.white')
+const blackKeys = document.querySelectorAll('.key.black')
 
+keys.forEach(key => {
+  key.addEventListener('click', () => playNote(key))
+})
+
+document.addEventListener('keydown', e => {
+    if (e.repeat) return
+    const key = e.key
+    const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+    const blackKeyIndex = BLACK_KEYS.indexOf(key)
+
+    if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
+    if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
+});
+    
 keys.forEach(key => {
     key.addEventListener('click', () => playNode(key));
 })
